2b7c13b0bcc27d21ac63b63cf883d66e0364dfcb
Fabio Reis <fabio.reis@qwasolucoes.com.br>
2023-12-13 23:27:32 -0300

    :sparkles: feat: adicionado campos no registro de usuários

--- src/App.tsx
+++ src/App.tsx
@@ -7,8 +7,7 @@ function App() {
 
   return (
     <>
-    <UserRegister /> 
-    <UserRegister /> 
+    <UserRegister />
     </>
   )
 }
--- src/components/UserRegister.tsx
+++ src/components/UserRegister.tsx
@@ -1,7 +1,15 @@
 function UserRegister() {
     return ( 
         <div>
+            <h3>
             Cadastro de usuários
+            </h3>
+            Nome completo: <input type="text" /><br />
+            Nome usuário: <input type="text" /><br />
+            Email: <input type="email"/><br />
+            Senha: <input type="password" /><br />
+            Repetir senha: <input type="password" /><br />
+            <button type="submit">Cadastrar</button>
         </div>
      );
 }
