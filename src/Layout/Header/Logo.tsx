30bfb712ae3b7596d43319f470ff066dfc503e43
Fabio Reis <fabio.reis@qwasolucoes.com.br>
2023-12-21 21:41:30 -0300

    :sparkles: feat: adicioando logo

---
+++ src/Layout/Header/Logo.tsx
@@ -0,0 +1,8 @@
+import styles from "./Logo.module.css"
+function Logo(){
+    return(
+        <img src="src/assets/react.svg" alt="React" className={styles.logo}/>
+    )
+}
+
+export default Logo
--- src/Layout/Header/NavBar.tsx
+++ src/Layout/Header/NavBar.tsx
@@ -1,10 +1,11 @@
 import styles from "./NavBar.module.css";
 import BotaoNavegacao from "./BotaoNavegacao";
+import Logo from "./Logo";
 
 function NavBar() {
   return (
     <nav className={styles.topnav}>
-      <img src="src/assets/react.svg" alt="React" />
+      <Logo />
       <BotaoNavegacao rota={"/contact"}>Contato</BotaoNavegacao>
       <BotaoNavegacao rota={"/about"}>Sobre</BotaoNavegacao>
       <BotaoNavegacao rota={"/"}>Home</BotaoNavegacao>
